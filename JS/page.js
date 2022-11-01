13da74dce7e046258614cacaffd7a1696c0e0bb8
cpsoucy <96217410+cpsoucy@users.noreply.github.com>
2022-10-31 20:08:35 -0400

    started media_gallery

---
+++ JS/page.js
@@ -0,0 +1,10 @@
+/* Checklist for page.js:
+    1. Javascript for mobile navigation (the nav bar shrinking down to one item each row)
+    2. Slider animation (?)
+    3. Lightbox modals
+*/
+
+// a test function that runs when the button is clicked
+function sampleFunction() {
+    alert("You clicked the button");
+  }
